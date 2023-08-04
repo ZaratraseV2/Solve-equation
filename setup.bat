ae44cb2b743068ed2e7f4d3162c07267df739a30
Zaratrase <122055075+ZaratraseV2@users.noreply.github.com>
2023-08-04 21:35:15 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,17 +1,11 @@
 @echo off
-
-
-@REM Installation des librairies pystyle et sympy
 pip install pystyle
 pip install sympy
-
-@REM Verification de l'installation
-pip show pystyle
 pip show sumpy
-echo Installation terminée.
 
 
-python equation.py
+
+
 
 
 
